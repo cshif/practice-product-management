ef858edcaea544d9f9ef2dafbc0259df71188432
cshif <cooshif@gmail.com>
2023-01-01 21:57:17 +0800

    refactor: mutate login status and session id when successfully logged in

--- controllers/login.js
+++ controllers/login.js
@@ -6,10 +6,15 @@ const loginController = {
     const sql = `SELECT * FROM user WHERE account = '${account}' AND password = '${password}'`;
     db.query(sql, (error, result) => {
       if (error) throw error;
-      if (result.length) {
-        req.sessionID.user = account;
-        res.send('successfully logged in!');
-      } else res.send('incorrect account or password');
+      if (result.length) loginController.loggedIn(req, res);
+      else res.send('incorrect account or password');
+    });
+  },
+  loggedIn: (req, res) => {
+    const sql = `UPDATE user SET loggedIn = 1, sessionId = '${req.sessionID}' WHERE account = '${req.body.account}' AND password = '${req.body.password}'`;
+    db.query(sql, error => {
+      if (error) throw error;
+      res.send('successfully logged in!');
     });
   },
   logout: (req, res) => {
