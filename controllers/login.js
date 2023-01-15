52ea5acf630aa54a38b68170849f35ac4d7eb625
cshif <cooshif@gmail.com>
2023-01-15 23:44:25 +0800

    remove unused code

--- controllers/login.js
+++ controllers/login.js
@@ -27,33 +27,7 @@ const loginController = {
       if (error) throw error;
       else next();
     });
-  },
-  // logout: (req, res) => {
-  //   const sql = `UPDATE user SET loggedIn = 0, token = null WHERE account = '${req.body.account}'`;
-  //   db.query(sql, async error => {
-  //     if (error) throw error;
-  //     res.locals.loggedInUser = {};
-  //     await fs.writeFile(
-  //       './user.json',
-  //       JSON.stringify({}),
-  //       error => {
-  //         if (error) throw error;
-  //       }
-  //     );
-  //     res.send('successfully logged out!');
-  //   });
-  // },
-  // register: (req, res) => {
-  //   const form = {
-  //     account: req.body.account,
-  //     password: req.body.password
-  //   };
-  //   const sql = 'INSERT INTO user SET ?';
-  //   db.query(sql, form, error => {
-  //     if (error) throw error;
-  //     res.send('successfully registered!');
-  //   });
-  // }
+  }
 };
 
 module.exports = loginController;
