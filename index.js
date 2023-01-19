1a40b1d37bcd7f2b5ff17d701467a0cb2f643ff0
cshif <cooshif@gmail.com>
2023-01-19 13:01:24 +0800

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const session = require('express-session');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(session({
